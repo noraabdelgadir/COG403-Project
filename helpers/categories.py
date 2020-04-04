98c80467c27124cb033fdc0e6baf44a099bac540
Nora Abdelgadir <nora.abdelgadir@mail.utoronto.ca>
2020-04-04 19:57:17 -0400

    add category constants for english and french

---
+++ helpers/categories.py
@@ -0,0 +1,214 @@
+# all categories
+CATEGORIES = ['insect', 'shape', 'weather', 'food', 'bird', 'mammal', 'colour', 'flower', 'country', 'sport', 'vehicle']
+FR_CATEGORIES = ['insecte', 'forme', 'météo', 'nourriture', 'oiseau', 'mammifère', 'couleurs', 'fleur', 'pays', 'sport', 'véhicule']
+EN_FR_CATEGORIES = {
+    'insect': 'insecte', 
+    'shape': 'forme', 
+    'weather': 'météo', 
+    'food': 'nourriture', 
+    'bird': 'oiseau', 
+    'mammal': 'mammifère', 
+    'colour': 'couleurs', 
+    'flower': 'fleur', 
+    'country': 'pays', 
+    'sport': 'sport', 
+    'vehicle': 'véhicule'
+}
+
+# insects
+INSECTS = ['firefly', 'spider', 'flea', 'ant', 'butterfly', 'locust', 'wasp', 'beetle', 'roach', 'fly', 'pupa', 'bee']
+FR_INSECTS = ['lucioles', 'araignée', 'puce', 'fourmi', 'papillon', 'criquet', 'guêpe', 'scarabée', 'cafard', 'mouche', 'moustique', 'abeille']
+EN_FR_INSECTS = {
+    'firefly': 'lucioles', 
+    'spider': 'araignée',
+    'flea': 'puce', 
+    'ant': 'fourmi', 
+    'butterfly': 'papillon', 
+    'locust': 'criquet', 
+    'wasp': 'guêpe', 
+    'beetle': 'scarabée', 
+    'roach': 'cafard', 
+    'fly': 'mouche', 
+    'mosquito': 'moustique',
+    'bee': 'abeille'
+}
+
+# shapes
+SHAPES = ['cylinder', 'line', 'rectangle', 'triangle', 'square', 'cube', 'circle', 'round', 'oval', 'circular', 'star', 'ellipse']
+FR_SHAPES = ['cylindre', 'ligne', 'rectangle', 'triangle', 'carré', 'cube', 'cercle', 'rond', 'ovale', 'circulaire', 'étoile', 'ellipse']
+EN_FR_SHAPES = {
+    'cylinder': 'cylindre', 
+    'line': 'ligne',
+    'rectangle': 'rectangle', 
+    'triangle': 'triangle',
+    'square': 'carré', 
+    'cube': 'cube',
+    'circle': 'cercle', 
+    'round': 'rond',
+    'oval': 'ovale',
+    'circular': 'circulaire',
+    'star': 'étoile',
+    'ellipse': 'ellipse'
+}
+
+# weather
+WEATHER = ['wind', 'precipitation', 'rain', 'tornado', 'fog', 'rainy', 'drought', 'sunny', 'snow', 'hail', 'cloud', 'cloudy']
+FR_WEATHER = ['vent', 'précipitation', 'pluie', 'tornade', 'brouillard', 'pluvieux', 'sécheresse', 'ensoleillé', 'neige', 'grêle', 'nuage', 'nuageux']
+EN_FR_WEATHER = {
+    'wind': 'vent',
+    'precipitation': 'précipitation', 
+    'rain': 'pluie', 
+    'tornado': 'tornade', 
+    'fog': 'brouillard', 
+    'rainy': 'pluvieux', 
+    'drought': 'sécheresse', 
+    'sunny': 'ensoleillé',
+    'snow': 'neige', 
+    'hail': 'grêle',
+    'cloud': 'nuage', 
+    'cloudy': 'nuageux'
+}
+
+# food
+FOOD = ['ham', 'spaghetti', 'cheese', 'salad', 'chocolate', 'steak', 'pasta', 'soup', 'butter', 'yogurt', 'noodles', 'bread']
+FR_FOOD = ['jambon', 'spaghetti', 'fromage', 'salade', 'chocolat', 'steak', 'pâtes', 'soupe', 'beurre', 'yaourt', 'nouilles', 'pain']
+EN_FR_FOOD = {
+    'ham': 'jambon', 
+    'spaghetti': 'spaghetti',
+    'cheese': 'fromage', 
+    'salad': 'salade',
+    'chocolate': 'chocolat',
+    'steak': 'steak', 
+    'pasta': 'pâtes', 
+    'soup': 'soupe', 
+    'butter': 'beurre', 
+    'yogurt': 'yaourt',
+    'noodles': 'nouilles', 
+    'bread': 'pain'
+}
+
+# birds
+BIRDS = ['robin', 'goose', 'ostrich', 'owl', 'parrot', 'falcon', 'eagle', 'flamingo', 'pigeon', 'finch', 'swan', 'penguin']
+FR_BIRDS = ['robin', 'oie', 'autruche', 'hibou', 'perroquet', 'faucon', 'aigle', 'flamingo', 'pigeon', 'finch', 'cygne', 'manchot']
+EN_FR_BIRDS = {
+    'robin': 'robin', 
+    'goose': 'oie', 
+    'ostrich': 'autruche', 
+    'owl': 'hibou', 
+    'parrot': 'perroquet', 
+    'falcon': 'faucon', 
+    'eagle': 'aigle', 
+    'flamingo': 'flamingo', 
+    'pigeon': 'pigeon', 
+    'finch': 'finch', 
+    'swan': 'cygne', 
+    'penguin': 'manchot'
+}
+
+# mammals
+MAMMALS = ['rabbit', 'beaver', 'horse', 'bear', 'deer', 'dog', 'elephant', 'camel', 'giraffe', 'raccoon', 'sheep', 'cow']
+FR_MAMMALS = ['lapin', 'castor', 'cheval', 'ours', 'cerf', 'chien', 'éléphant', 'chameau', 'girafe', 'raton', 'mouton', 'vache']
+EN_FR_MAMMALS = {
+    'rabbit': 'lapin', 
+    'beaver': 'castor', 
+    'horse': 'cheval',  
+    'bear': 'ours', 
+    'deer': 'cerf', 
+    'dog': 'chien', 
+    'elephant': 'éléphant', 
+    'camel': 'chameau', 
+    'giraffe': 'girafe', 
+    'raccoon': 'raton', 
+    'sheep': 'mouton', 
+    'cow': 'vache'
+}
+
+# colours
+COLOURS = ['purple', 'blue', 'white', 'orange', 'green', 'red', 'yellow', 'cyan', 'mauve', 'magenta', 'beige', 'azure']
+FR_COLOURS = ['pourpre', 'bleu', 'blanc', 'orange', 'vert', 'rouge', 'jaune', 'cyan', 'mauve', 'magenta', 'beige', 'azur']
+EN_FR_COLOURS = {
+    'purple': 'pourpre',
+    'blue': 'bleu', 
+    'white': 'blanc', 
+    'orange': 'orange', 
+    'green': 'vert', 
+    'red': 'rouge', 
+    'yellow': 'jaune', 
+    'cyan': 'cyan', 
+    'mauve': 'mauve', 
+    'magenta': 'magenta', 
+    'beige': 'beige', 
+    'azure': 'azur'
+}
+
+# flowers
+FLOWERS = ['dandelion', 'iris', 'petunia', 'orchid', 'daisy', 'sunflower', 'tulip', 'poppy', 'violet', 'lotus', 'lily', 'rose']
+FR_FLOWERS = ['pissenlit', 'iris', 'pétunia', 'orchidée', 'marguerite', 'tournesol', 'tulipe', 'pavot', 'violet', 'lotus', 'lys', 'rose']
+EN_FR_FLOWERS = {
+    'dandelion': 'pissenlit', 
+    'iris': 'iris', 
+    'petunia': 'pétunia', 
+    'orchid': 'orchidée', 
+    'daisy': 'marguerite',
+    'sunflower': 'tournesol', 
+    'tulip': 'tulipe', 
+    'poppy': 'pavot', 
+    'violet': 'violet', 
+    'lotus': 'lotus',
+    'lily': 'lys',
+    'rose': 'rose',
+}
+
+# countries
+COUNTRIES = ['china', 'america', 'switzerland', 'norway', 'denmark', 'italy', 'england', 'argentina', 'finland', 'spain', 'france', 'canada']
+FR_COUNTRIES = ['chine', 'amérique', 'suisse', 'norvège', 'danemark', 'italie', 'angleterre', 'argentine', 'finlande', 'espagne', 'france', 'canada']
+EN_FR_COUNTRIES = {
+    'china': 'chine', 
+    'america': 'amérique', 
+    'switzerland': 'suisse', 
+    'norway': 'norvège', 
+    'denmark': 'danemark', 
+    'italy': 'italie', 
+    'england': 'angleterre', 
+    'argentina': 'argentine', 
+    'finland': 'finlande', 
+    'spain': 'espagne', 
+    'france': 'france', 
+    'canada': 'canada'
+}
+
+# sports
+SPORTS = ['snowboarding', 'football', 'badminton', 'tennis', 'soccer', 'volleyball', 'skating', 'cycling', 'golf', 'baseball', 'swimming', 'hockey']
+FR_SPORTS = ['snowboard', 'football', 'badminton', 'tennis', 'football', 'volley', 'patinage', 'cyclisme', 'golf', 'baseball', 'nager', 'hockey']
+EN_FR_SPORTS = {
+    'snowboarding': 'snowboard', 
+    'football': 'football', 
+    'badminton': 'badminton', 
+    'tennis': 'tennis', 
+    'soccer': 'football', 
+    'volleyball': 'volley',
+    'skating': 'patinage', 
+    'cycling': 'cyclisme', 
+    'golf': 'golf', 
+    'baseball': 'baseball', 
+    'swimming': 'nager', 
+    'hockey': 'hockey'
+}
+
+# vehicles
+VEHICLES = ['truck', 'airplane', 'boat', 'bicycle', 'car', 'van', 'tractor', 'canoe', 'bus', 'taxi', 'train', 'motorcycle']
+FR_VEHICLES = ['camion', 'avion', 'bateau', 'vélo', 'voiture', 'camionnette', 'tracteur', 'canoë', 'bus', 'taxi', 'train', 'moto']
+EN_FR_VEHICLES = {
+    'truck': 'camion', 
+    'airplane': 'avion', 
+    'boat': 'bateau', 
+    'bicycle': 'vélo',
+    'car': 'voiture', 
+    'van': 'camionnette',
+    'tractor': 'tracteur', 
+    'canoe': 'canoë',
+    'bus': 'bus', 
+    'taxi': 'taxi', 
+    'train': 'train', 
+    'motorcycle': 'moto'
+}
